9aac04e7a276b140460ca6f1dab71c0da5708542
notyy <notyycn@gmail.com>
2019-09-19 15:21:17 +0800

    add a NoInput input pattern, so that we can define constant input end, or mock inputendpoint

--- src/main/scala/com/notyy/visualfp/domain/InputPattern.scala
+++ src/main/scala/com/notyy/visualfp/domain/InputPattern.scala
@@ -3,5 +3,5 @@ package com.notyy.visualfp.domain
 trait InputPattern
 
 case object OneOfInput extends InputPattern
-
 case object AllInput extends InputPattern
+case object NoInput extends InputPattern
