4332bcaf11bb5df675cc59b73b7199ba2bc75798
notyy <notyycn@gmail.com>
2019-10-16 16:47:16 +0800

    format code

--- src/main/scala/com/github/notyy/typeflow/editor/AddDefinition.scala
+++ src/main/scala/com/github/notyy/typeflow/editor/AddDefinition.scala
@@ -5,9 +5,9 @@ import com.github.notyy.typeflow.domain.{Definition, Function, InputEndpoint, Mo
 object AddDefinition {
   def execute(savedModel: Model, addDefinitionCommand: AddDefinitionCommand): Model = {
     val newDefinition: Definition = addDefinitionCommand match {
-      case AddInputEndpointCommand(modelName,name,outputType) => InputEndpoint(name,outputType)
-      case AddFunctionCommand(modelName, name, inputs, outputs) => Function(name, inputs,outputs)
-      case AddOutputEndpointCommand(modelName,name,inputType,outputType,errorOutput) => OutputEndpoint(name, inputType,outputType,errorOutput)
+      case AddInputEndpointCommand(modelName, name, outputType) => InputEndpoint(name, outputType)
+      case AddFunctionCommand(modelName, name, inputs, outputs) => Function(name, inputs, outputs)
+      case AddOutputEndpointCommand(modelName, name, inputType, outputType, errorOutput) => OutputEndpoint(name, inputType, outputType, errorOutput)
     }
     savedModel.copy(definitions = savedModel.definitions.appended(newDefinition))
   }
