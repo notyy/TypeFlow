5c8703235b531043bd83fcb77906f14f57f74e99
notyy <notyycn@gmail.com>
2019-10-16 15:57:07 +0800

    add a unit test for AddDefinition

--- src/test/scala/com/github/notyy/typeflow/editor/AddDefinitionTest.scala
+++ src/test/scala/com/github/notyy/typeflow/editor/AddDefinitionTest.scala
@@ -1,12 +1,14 @@
 package com.github.notyy.typeflow.editor
 
-import com.github.notyy.typeflow.domain.Model
+import com.github.notyy.typeflow.domain.{Model, OutputType}
 import org.scalatest.{FunSpec, Matchers}
 
 class AddDefinitionTest extends FunSpec with Matchers {
   describe("AddDefinition"){
     it("allow's user to add new Definition to a given model"){
       val model = Model("testModel", Vector.empty,Vector.empty,None)
+      val newModel = AddDefinition.execute(model, AddInputEndpointCommand("testModel","UserInputEndpoint",OutputType("InputType")))
+      newModel.definitions.head.name shouldBe "UserInputEndpoint"
     }
   }
 }
