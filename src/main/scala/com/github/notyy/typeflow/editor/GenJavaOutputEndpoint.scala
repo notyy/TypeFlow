3ef48dcd0dbe60dc246bc61754adddea97a060bb
notyy <notyycn@gmail.com>
2019-11-09 20:37:41 +0800

    complete GenJavaOutputEndpoint

---
+++ src/main/scala/com/github/notyy/typeflow/editor/GenJavaOutputEndpoint.scala
@@ -0,0 +1,17 @@
+package com.github.notyy.typeflow.editor
+
+import com.github.notyy.typeflow.domain.OutputEndpoint
+
+class GenJavaOutputEndpoint(val genFormalParams: GenFormalParams) {
+  def execute(packageName: PackageName, outputEndpoint: OutputEndpoint, codeTemplate: CodeTemplate): JavaCode = {
+    val code = codeTemplate.value.replaceAllLiterally("$PackageName$", packageName.value).
+      replaceAllLiterally("$DefinitionName$", outputEndpoint.name).
+      replaceAllLiterally("$ReturnType$", replaceEmptyReturnTypeWithVoid(outputEndpoint.outputs.head.outputType.name)).
+      replaceAllLiterally("$Params$", genFormalParams.execute(outputEndpoint.inputs))
+    JavaCode(s"${packageName.value}.${outputEndpoint.name}",code)
+  }
+
+  def replaceEmptyReturnTypeWithVoid(returnType: String): String = {
+    if(returnType == "Unit") "void" else returnType
+  }
+}
---
+++ src/test/scala/com/github/notyy/typeflow/editor/GenJavaOutputEndpointTest.scala
@@ -0,0 +1,39 @@
+package com.github.notyy.typeflow.editor
+
+import com.github.notyy.typeflow.domain.{Input, InputType, OutputEndpoint, OutputType}
+import org.scalatest.{FunSpec, Matchers}
+
+class GenJavaOutputEndpointTest extends FunSpec with Matchers {
+  describe("GenJavaOutputEndpoint") {
+    it("should generate java code skeleton for output endpoint") {
+      val packageName = PackageName("com.github.notyy.typeflow.editor")
+      val print: OutputEndpoint = OutputEndpoint(
+        "Print",
+        Vector(Input(InputType("Integer"),1)),
+        OutputType("Unit"),
+        Vector.empty
+      )
+      val codeTemplate =
+        """|package $PackageName$;
+           |
+           |public class $DefinitionName$ {
+           |    public $ReturnType$ execute($Params$) {
+           |        System.out.println(param1);
+           |    }
+           |}
+           |""".stripMargin
+      val genJavaOutputEndpoint = new GenJavaOutputEndpoint(new GenFormalParams)
+      val printCode = genJavaOutputEndpoint.execute(packageName, print, CodeTemplate(codeTemplate))
+      printCode.qualifiedName shouldBe "com.github.notyy.typeflow.editor.Print"
+      printCode.content shouldBe
+        """|package com.github.notyy.typeflow.editor;
+           |
+           |public class Print {
+           |    public void execute(Integer param1) {
+           |        System.out.println(param1);
+           |    }
+           |}
+           |""".stripMargin
+    }
+  }
+}
