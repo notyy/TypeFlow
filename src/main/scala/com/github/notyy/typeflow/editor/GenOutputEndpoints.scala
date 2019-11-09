a02a5136c87ff6deef3554381c5b7387b34e9a06
notyy <notyycn@gmail.com>
2019-11-09 20:41:13 +0800

    complete GenOutputEndpoints for java code

---
+++ src/main/scala/com/github/notyy/typeflow/editor/GenOutputEndpoints.scala
@@ -0,0 +1,12 @@
+package com.github.notyy.typeflow.editor
+
+import com.github.notyy.typeflow.domain.OutputEndpoint
+
+class GenOutputEndpoints(val genJavaOutputEndpoint: GenJavaOutputEndpoint) {
+  def execute(codeLang: CodeLang, outputEndpoints: Vector[OutputEndpoint], packageName: PackageName, codeTemplate: CodeTemplate): Vector[JavaCode] = {
+    codeLang match {
+      case JAVA_LANG => outputEndpoints.map(oe => genJavaOutputEndpoint.execute(packageName, oe, codeTemplate))
+      case SCALA_LANG => ???
+    }
+  }
+}
