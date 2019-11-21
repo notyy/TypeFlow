e26164e2dc249b136ef47c1270e1a3f97ba16db3
notyy <notyycn@gmail.com>
2019-11-21 21:28:59 +0800

    strange, what wrong?

--- src/main/scala/com/github/notyy/typeflow/editor/GenCallingChain.scala
+++ src/main/scala/com/github/notyy/typeflow/editor/GenCallingChain.scala
@@ -1,6 +1,6 @@
 package com.github.notyy.typeflow.editor
 
-import com.github.notyy.typeflow.domain.{Connection, Definition, Instance}
+import com.github.notyy.typeflow.domain.{Connection, Instance}
 import com.github.notyy.typeflow.util.TypeUtil
 import com.typesafe.scalalogging.Logger
 
@@ -15,41 +15,36 @@ class GenCallingChain(val genCallStatement: GenCallStatement) {
   private val waitingParams: scala.collection.mutable.Map[InstanceId, Map[InputIndex, ParamName]] = scala.collection.mutable.Map.empty
   private var startInstance: Option[Instance] = None
 
-  def execute(outFrom: Instance, outputIndex: Int, outputParamName: String, connections: Vector[Connection], instances: Vector[Instance],statements: Vector[String]): Vector[String] = {
+  def execute(outFrom: Instance, outputIndex: Int, outputParamName: String, connections: Vector[Connection], instances: Vector[Instance], statements: Vector[String]): Vector[String] = {
     startInstance = Some(outFrom)
-    prettifyStatements(accuStatements(outFrom,outputIndex,outputParamName,connections,instances,statements))
+    prettifyStatements(accuStatements(outFrom, Map(outputIndex -> outputParamName), connections, instances, statements))
   }
 
   private def prettifyStatements(statements: Vector[String]): Vector[String] = statements.map(s => s"      $s")
 
-  private def accuStatements(outFrom: Instance, outputIndex: Int, outputParamName: String, connections: Vector[Connection], instances: Vector[Instance], currStatements: Vector[String]): Vector[String] = {
-    val connsFromThis = connections.filter(conn => conn.fromInstanceId == outFrom.id && conn.outputIndex == outputIndex)
+  private def accuStatements(outFrom: Instance, outputParams: Map[Int, String], connections: Vector[Connection], instances: Vector[Instance], currStatements: Vector[String]): Vector[String] = {
+    val connsFromThis = connections.filter(conn => conn.fromInstanceId == outFrom.id)
     if (connsFromThis.isEmpty) {
-      logger.warn(s"no connection found for ${outFrom.id}.$outputIndex")
+      logger.warn(s"no connection found for ${outFrom.id}")
       currStatements
     } else {
       connsFromThis.flatMap { conn =>
+        val paramName = outputParams(conn.outputIndex)
         val targetInstanceId = conn.toInstanceId
         val targetInstanceInputIndex = conn.inputIndex
         val targetInstance = instances.find(_.id == targetInstanceId).get
-        val targetDefinition = targetInstance.definition
-        if(targetInstance == startInstance.get) {
-          val statement: String = genResponse4request(outputParamName)
+        if (targetInstance == startInstance.get) {
+          val statement: String = genResponse4request(paramName)
           currStatements.appended(statement)
         } else {
-          val resultName = TypeUtil.firstCharToLowercase(s"${targetInstanceId}Result")
+          val resultNamesMap = genResultName(targetInstance)
+          val resultName = if (resultNamesMap.size == 1) resultNamesMap.head._2
+          else s"(${resultNamesMap.values.mkString(",")})"
           //TODO only support java for now
-          val targetOutputs = targetDefinition.outputs
-          if (targetOutputs.size == 1) {
-            val statement = genCallStatements(outputParamName, targetInstance, targetInstanceInputIndex, resultName)
-            if (statement.isDefined) {
-              accuStatements(targetInstance, 1, resultName, connections, instances, currStatements.appended(statement.get))
-            } else {
-              currStatements
-            }
+          val statement = genCallStatements(paramName, targetInstance, targetInstanceInputIndex, resultName)
+          if (statement.isDefined) {
+            accuStatements(targetInstance, resultNamesMap, connections, instances, currStatements.appended(statement.get))
           } else {
-            //TODO complete this
-            logger.error("multiple output not supported yet")
             currStatements
           }
         }
@@ -57,26 +52,31 @@ class GenCallingChain(val genCallStatement: GenCallStatement) {
     }
   }
 
+  private def genResultName(targetInstance: Instance): Map[Int,String] = {
+    targetInstance.definition.outputs.filterNot(ot => TypeUtil.removeDecorate(ot.outputType.name) == "Unit").map { o =>
+      o.index -> TypeUtil.firstCharToLowercase(s"${targetInstance.id}Result${o.index}")
+    }.toMap
+  }
+
   private def genResponse4request(outputParamName: String) = {
     val statement = s"setResponse($outputParamName)"
     statement
   }
 
-  private def genCallStatements(outputParamName: String, targetInstance: Instance, targetInstanceInputIndex: Int, resultName: String): Option[String] = {
+  private def genCallStatements(paramNames: String, targetInstance: Instance, targetInstanceInputIndex: Int, resultName: String): Option[String] = {
     val targetDefinition = targetInstance.definition
     val targetInputs = targetDefinition.inputs
     if (targetInputs.size == 1) {
-      genCallStatement.execute(Vector(outputParamName), resultName, targetDefinition)
+      genCallStatement.execute(Vector(paramNames), resultName, targetDefinition)
     } else {
       if (waitingParams.contains(targetInstance.id)) {
         val prevParams = waitingParams(targetInstance.id)
-        val currParams = prevParams + (targetInstanceInputIndex -> outputParamName)
+        val currParams = prevParams + (targetInstanceInputIndex -> paramNames)
         if (currParams.size == targetInstance.definition.inputs.size) {
           //enough parameters
-          //TODO should keep params here and let callee to solve multiple params
           val params = currParams.toVector.sortBy(_._1).map(_._2)
           //          val statement = s"val $resultName = new ${targetDefinition.name}().execute($params)"
-          val statement = genCallStatement.execute(params, resultName,targetDefinition)
+          val statement = genCallStatement.execute(params, resultName, targetDefinition)
           waitingParams.remove(targetInstance.id)
           statement
         } else {
@@ -84,7 +84,7 @@ class GenCallingChain(val genCallStatement: GenCallStatement) {
           None
         }
       } else {
-        waitingParams += (targetInstance.id -> Map(targetInstanceInputIndex -> outputParamName))
+        waitingParams += (targetInstance.id -> Map(targetInstanceInputIndex -> paramNames))
         None
       }
     }
